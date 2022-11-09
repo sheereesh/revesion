2b1fac7a42481cf50b9c2db178a176cd969ba310
dumbu1 <krishnamohan.balivada@gmail.com>
2022-11-09 17:03:15 +0530

    hooks added

--- revision/src/test/java/stepDefinations/Page1SD.java
+++ revision/src/test/java/stepDefinations/Page1SD.java
@@ -7,18 +7,15 @@ public class Page1SD {
 	
 	@Given("user is on {string} page")
 	public void user_is_on_page(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    System.out.println(System.getProperty("user.dir"));
 	}
 	@When("click on {string} link")
 	public void click_on_link(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    System.out.println("when");
 	}
 	@Then("system will display {string} page")
 	public void system_will_display_page(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	  System.out.println("then");
 	}
 
 
--- revision/src/test/java/utility/PropertyReader.java
+++ revision/src/test/java/utility/PropertyReader.java
@@ -1,5 +1,42 @@
 package utility;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyReader {
+	/**
+	 * 
+	 * @param fileName
+	 * @return Properties
+	 */
+	
+	public static Properties getAllProperties(String fileName) {
+		
+		FileInputStream inputFile;
+		Properties p=null;
+		try {
+			inputFile=new FileInputStream(System.getProperty("user.dir")+"/sre/test/resources/properties/"+fileName+".properties");
+		
+		p =new Properties();
+		try {
+			p.load(inputFile);
+		} catch (IOException e) {
+			System.out.println(" not a valid data in property file");
+		}
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("not a valid property file name or file path ");
+		}
+		
+		
+		return p;
+	}
+	
+	public static String getPropertyValue(String propertyFileName,String propertyKey) {
+		
+		return getAllProperties(propertyFileName).getProperty(propertyKey);
+	}
 
 }
