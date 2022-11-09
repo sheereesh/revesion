eb21ccefb8d9e5bb42ca21ef0c089472da089f43
dumbu1 <krishnamohan.balivada@gmail.com>
2022-11-09 13:16:18 +0530

    first commit

---
+++ revision/src/test/java/stepDefinations/Page1SD.java
@@ -0,0 +1,25 @@
+package stepDefinations;
+
+import io.cucumber.java.en.*;
+
+public class Page1SD {
+	
+	
+	@Given("user is on {string} page")
+	public void user_is_on_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("click on {string} link")
+	public void click_on_link(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("system will display {string} page")
+	public void system_will_display_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
---
+++ revision/src/test/resources/features/page1/feature1.feature
@@ -0,0 +1,5 @@
+Feature: feature1
+Scenario: scenerio1
+Given user is on "Web Examples - CrossBrowserTesting.com" page
+When click on "Selenium Example Link" link
+Then system will display "Selenium Test Example Page" page
